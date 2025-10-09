ba5871df0c7ee664e1c5cb9e5a0b9ffec3ac426d
Patiess <juszkopatrik@gmail.com>
2025-10-09 03:22:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Hello CI/CD World!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.get("/")
+def hello():
+    return "Hello from CI/CD on Kubernetes! 🎉"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
