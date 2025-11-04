ccf83c225e6c64198fcf49a9f7f21782c4fb7b22
Patiess <juszkopatrik@gmail.com>
2025-11-04 14:56:39 +0100

    szakdolgozat update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Ez az automatikus CI/CD folyamat v2.0!"
+    return "hello szia szakdolgozat"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
