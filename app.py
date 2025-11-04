c6e275bdef83388dfc9eb6535345f9e4b205ceac
Patiess <juszkopatrik@gmail.com>
2025-11-04 14:52:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from CI/CD on Kubernetes! 🎉"
+    return "Ez az automatikus CI/CD folyamat v2.0!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
