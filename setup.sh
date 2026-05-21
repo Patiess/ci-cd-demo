f19f349c87f14c3f98390cae2b002d8e2fc68466
Juszkó Patrik <juszkopatrik@Juszko-MacBook-Air.local>
2026-05-21 08:14:48 +0200

    Jenkins varazslo kikapcsolasa es automatikus job betoltes

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+
+echo "Kubernetes konfiguráció másolása..."
 mkdir -p ./jenkins_home/.kube
 cp ~/.kube/config ./jenkins_home/.kube/config
-# Kicseréljük az IP-ket, hogy a konténer lássa a Mac-et
+
 sed -i '' 's/127.0.0.1/host.docker.internal/g' ./jenkins_home/.kube/config
 sed -i '' 's/localhost/host.docker.internal/g' ./jenkins_home/.kube/config
-echo "Környezet előkészítve a szakdolgozathoz!"
+
+echo "Jenkins job visszaállítása..."
+mkdir -p ./jenkins_home/jobs/szakdolgozat-pipeline
+cp ./jenkins-jobs-backup/szakdolgozat-pipeline-config.xml ./jenkins_home/jobs/szakdolgozat-pipeline/config.xml
+
+echo "Környezet és Jenkins job sikeresen előkészítve a vizsgához!"
